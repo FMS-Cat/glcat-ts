fe153cd41743c673a8ce6d00a62265d612ca9555
FMS-Cat <yutaka.obuchi@gmail.com>
2019-02-24 01:15:38 +0900

    hmm

--- .circleci/prepare-image.js
+++ .circleci/prepare-image.js
@@ -1,3 +1,18 @@
-require( 'child_process' ).execSync( `\
-docker build -t fmscat/node-with-xvfb . && \
-docker push fmscat/node-with-xvfb` );
+const cp = require( 'child_process' );
+
+const run = ( command ) => {
+  return new Promise( ( resolve, reject ) => {
+    const p = cp.exec( command, ( err ) => {
+      if ( err ) { reject( err ); }
+      else { resolve(); }
+    } );
+    p.stdout.pipe( process.stdout );
+    p.stderr.pipe( process.stderr );
+  } );
+};
+
+run(
+  'docker build -t fmscat/node-with-xvfb .'
+).then( run(
+  'docker push fmscat/node-with-xvfb'
+) );
