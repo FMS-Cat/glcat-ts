613af0d99cce5bab9692b152b6fc3f389aee4f16
FMS-Cat <yutaka.obuchi@gmail.com>
2019-02-25 00:26:23 +0900

    kill me

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = ( env, argv ) => {
 
   return {
     entry: {
-      'glcat.ts': './src/index.ts'
+      'glcat.js': './src/index.ts'
     },
     output: {
       path: path.join( __dirname, 'dist' ),
