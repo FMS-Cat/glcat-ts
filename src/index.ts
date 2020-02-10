32228d52dc46c986ebb4d18c3308fb90c3b47231
FMS-Cat <yutaka.obuchi@gmail.com>
2020-02-10 14:09:31 +0900

    WOW I forgot to export GLCatShader

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,5 @@ export * from './GLCatBuffer';
 export * from './GLCatFramebuffer';
 export * from './GLCatProgram';
 export * from './GLCatRenderbuffer';
+export * from './GLCatShader';
 export * from './GLCatTexture';
